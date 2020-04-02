8df5d11ff4f2385c0bd45e7ed23502622bea211d
Katherine Chin <k@alumni.bentley.edu>
2020-04-02 07:01:32 -0400

    more layout updates

--- app/src/main/java/com/example/tvcr/Contact.java
+++ app/src/main/java/com/example/tvcr/Contact.java
@@ -1,3 +1,18 @@
+/*
+ * TVCR • CONTACT PAGE
+ * By Boxin Cao, Katherine Chin, & Shahzeb Khurshid
+ * Created in April 2020
+ * CS680 Pepe • Final Project
+ *
+ * Utilizing smartphones’ NFC readers, The Virtual Card Reader (TVCR) collects data
+ * from identification cards embedded with NFC tags, and the data is populated into
+ * a contacts list. The goal is to provide a simple, efficient way for professionals
+ * to exchange contact information.
+ *
+ *
+ *
+ */
+
 package com.example.tvcr;
 
 public class Contact {
--- app/src/main/java/com/example/tvcr/Home.java
+++ app/src/main/java/com/example/tvcr/Home.java
@@ -1,8 +1,43 @@
+/*
+ * TVCR • HOME PAGE
+ * By Boxin Cao, Katherine Chin, & Shahzeb Khurshid
+ * Created in April 2020
+ * CS680 Pepe • Final Project
+ *
+ * Utilizing smartphones’ NFC readers, The Virtual Card Reader (TVCR) collects data
+ * from identification cards embedded with NFC tags, and the data is populated into
+ * a contacts list. The goal is to provide a simple, efficient way for professionals
+ * to exchange contact information.
+ *
+ * The home page displays a ListView  of all contact names. The menu bar has options
+ * for searching contacts and adding contacts.
+ *
+ */
+
 package com.example.tvcr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -11,4 +46,7 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+
+
+
 }
